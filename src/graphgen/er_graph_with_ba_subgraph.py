dca9888e7bce503f74a4166548803fb71930850e
Harshit Kumar <harshit.kumar@jax.org>
2023-09-26 17:16:48 -0400

    Make graph models configurable

--- src/graphgen/er_graph_with_ba_subgraph.py
+++ src/graphgen/er_graph_with_ba_subgraph.py
@@ -5,7 +5,9 @@ import matplotlib.colors as mcolors
 import numpy as np
 import logging
 
-def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
+def create_er_replace_with_ba_adjacency(base_graph_model='ER',
+                                        motif_graph_model='BA',
+                                        n=300, p=0.2, m=3,
                                         min_motif_size=10, max_motif_size=30,
                                         min_num_motifs=3, max_num_motifs=5,
                                         motif_overlap=False,
@@ -18,6 +20,8 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
     a specified number of nodes with Barabási-Albert (BA) model motifs.
 
     Parameters:
+    - base_graph_model (str): The base graph model to use ('ER' for Erdős-Rényi or 'BA' for Barabási-Albert).
+    - motif_graph_model (str): The motif graph model to use.
     - n (int): Number of nodes in the Erdős-Rényi (ER) graph.
     - p (float): Probability of an edge between nodes in the ER graph (0 <= p <= 1).
     - m (int): Number of edges to attach from a new node to existing nodes in the BA model (m >= 1).
@@ -37,6 +41,7 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
     - np.ndarray or None: The resulting adjacency matrix of the modified graph, or None if invalid parameters.
     """
     if log:  # Check if logging is enabled
+        logging.info(f"base_graph_model= {base_graph_model}, motif_graph_model= {motif_graph_model}")
         logging.info(f"n = {n}, p = {p}, m = {m}, min_motif_size = {min_motif_size}, max_motif_size = {max_motif_size}")
         logging.info(f"min_num_motifs = {min_num_motifs}, max_num_motifs = {max_num_motifs}, motif_overlap = {motif_overlap}")
         logging.info(f"min_base_edge_weight = {min_base_edge_weight}, max_base_edge_weight = {max_base_edge_weight}")
@@ -44,7 +49,10 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
         logging.info(f"plot = {plot}")
 
     # Create the ER graph adjacency matrix
-    er_graph = nx.erdos_renyi_graph(n, p)
+    if base_graph_model == 'ER':
+        er_graph = nx.erdos_renyi_graph(n, p)
+    elif base_graph_model == 'BA':
+        er_graph = nx.barabasi_albert_graph(n, m)
     er_adjacency = nx.to_numpy_array(er_graph)
     # replace edge weights
     if min_base_edge_weight != max_base_edge_weight:
@@ -65,14 +73,14 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
         
     # check degree 0 nodes in the original ER graph
     if np.sum(er_adjacency.sum(axis=1) == 0) > 0:
-        logging.info("Original ER graph contains nodes with degree 0.")
+        logging.info(f"Original {base_graph_model} graph contains nodes with degree 0.")
 
     # plot original ER graph
     if plot:
         plt.figure(figsize=(14, 8))
         plt.subplot(221)
         nx.draw_networkx(nx.Graph(er_adjacency), with_labels=True, node_color='skyblue', node_size=300)
-        plt.title('Original ER graph')
+        plt.title(f'Original {base_graph_model} graph')
 
     # Randomly determine the number of motifs to embed
     num_motifs = random.randint(min_num_motifs, max_num_motifs)
@@ -109,7 +117,10 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
 
         # Create the BA model adjacency matrix for the motif
         if ba_n >= m:
-            ba_motif = nx.to_numpy_array(nx.barabasi_albert_graph(ba_n, m))
+            if motif_graph_model == 'ER':
+                ba_motif = nx.to_numpy_array(nx.erdos_renyi_graph(ba_n, p))
+            elif motif_graph_model == 'BA':
+                ba_motif = nx.to_numpy_array(nx.barabasi_albert_graph(ba_n, m))
 
             # replace edge weights
             if min_motif_edge_weight != max_motif_edge_weight:
@@ -175,11 +186,12 @@ def create_er_replace_with_ba_adjacency(n=300, p=0.2, m=3,
         pos = nx.spring_layout(g)  # Position nodes using a layout algorithm
         edge_color_l = [edge_colors.get((u, v), 'gray') for u, v in g.edges()]
         nx.draw_networkx(g, pos, with_labels=True, node_color=node_colors, node_size=300, edge_color=edge_color_l)
-        plt.title(f'ER graph with {num_motifs} BA subgraphs')
+        plt.title(f'{base_graph_model} graph with {num_motifs} {motif_graph_model} subgraphs')
 
         # Plot degree distribution
         plt.subplot(223)
-        for g, l in zip([er_graph, nx.Graph(er_adjacency)], ['ER', 'ER with BA subgraphs']):
+        for g, l in zip([er_graph, nx.Graph(er_adjacency)], 
+                        [f'{base_graph_model}', f'{base_graph_model} with {motif_graph_model} subgraphs']):
             plt.plot(np.sort([j for _, j in g.degree()])[::-1], marker='.', label=l)
         plt.xlabel('Degree')
         plt.ylabel('Frequency')
