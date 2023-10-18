dbd623bab14ec25769bb2ea953832e06e2861401
Harshit Kumar <harshit.kumar@jax.org>
2023-10-18 14:23:08 -0400

    Add jaccard similarity

--- src/graphgen/graph_analyzer.py
+++ src/graphgen/graph_analyzer.py
@@ -37,7 +37,7 @@ class GraphAnalyzer:
             self.pc_model = self.generate_powerlaw_cluster()
             self.graph_visualizer = GraphVisualizer(
                 [self.graph, self.er_model, self.ba_model, self.ws_model, self.pc_model], 
-                [self.dataset_name, 'Erdos-Renyi', 'Barabasi-Albert', 'Watts-Strogatz', 'Powerlaw Cluster'],
+                [self.dataset_name, 'Erdos Renyi', 'Barabasi Albert', 'Watts Strogatz', 'Powerlaw Cluster'],
                 ['skyblue', 'lightcoral', 'lightgreen', 'pink', 'lavender'])
 
     def load_graph(self):
@@ -79,7 +79,7 @@ class GraphAnalyzer:
         """
         return self.graph
         
-    def _calculate_graph_properties(self, graph):
+    def _calculate_graph_properties(self, graph, graph_name):
         """
         Calculate the graph properties for the given graph.
         """
@@ -100,6 +100,7 @@ class GraphAnalyzer:
              # Assortativity is a measure of the tendency of nodes in a graph to be connected to other nodes with similar degrees. 
              # In genetic datasets, assortativity can be used to identify genes that are highly connected to other genes with similar expression levels.
             "Assortativity": f'{nx.degree_assortativity_coefficient(graph):.3f}',
+            "Jaccard Similarity": f'{self.calculate_jaccard_similarity()[graph_name]:.3f}'
         }
         return properties
 
@@ -107,11 +108,11 @@ class GraphAnalyzer:
         """
         Calculate the graph properties for the loaded graph, Erdos-Renyi, Barabasi-Albert, and Watts-Strogatz models.
         """
-        properties = {'Loaded Graph': self._calculate_graph_properties(self.graph),
-                      'Erdos-Renyi': self._calculate_graph_properties(self.er_model),
-                      'Barabasi-Albert': self._calculate_graph_properties(self.ba_model),
-                      'Watts-Strogatz': self._calculate_graph_properties(self.ws_model),
-                      'Powerlaw Cluster': self._calculate_graph_properties(self.pc_model)
+        properties = {'Loaded Graph': self._calculate_graph_properties(self.graph, 'Loaded Graph'),
+                      'Erdos Renyi': self._calculate_graph_properties(self.er_model, 'Erdos Renyi'),
+                      'Barabasi Albert': self._calculate_graph_properties(self.ba_model, 'Barabasi Albert'),
+                      'Watts Strogatz': self._calculate_graph_properties(self.ws_model, 'Watts Strogatz'),
+                      'Powerlaw Cluster': self._calculate_graph_properties(self.pc_model, 'Powerlaw Cluster'),
                       }
         return properties
 
@@ -192,9 +193,10 @@ class GraphAnalyzer:
 
         # Extract the Jaccard similarity coefficients
         jaccard_coefficients = {
-            'Erdos-Renyi': sum([coeff for _, _, coeff in jaccard_er]) / len(self.er_model.edges()),
-            'Barabasi-Albert': sum([coeff for _, _, coeff in jaccard_ba]) / len(self.ba_model.edges()),
-            'Watts-Strogatz': sum([coeff for _, _, coeff in jaccard_ws]) / len(self.ws_model.edges()),
+            'Loaded Graph': 1.0,
+            'Erdos Renyi': sum([coeff for _, _, coeff in jaccard_er]) / len(self.er_model.edges()),
+            'Barabasi Albert': sum([coeff for _, _, coeff in jaccard_ba]) / len(self.ba_model.edges()),
+            'Watts Strogatz': sum([coeff for _, _, coeff in jaccard_ws]) / len(self.ws_model.edges()),
             'Powerlaw Cluster': sum([coeff for _, _, coeff in jaccard_pc]) / len(self.pc_model.edges())
             }
 
