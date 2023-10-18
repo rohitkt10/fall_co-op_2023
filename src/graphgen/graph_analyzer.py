34c785510891b6bf6b2e4c4d80d6b6e342e8e4d3
Harshit Kumar <harshit.kumar@jax.org>
2023-10-18 11:48:47 -0400

    Add powerlaw cluster graph

--- src/graphgen/graph_analyzer.py
+++ src/graphgen/graph_analyzer.py
@@ -8,7 +8,7 @@ class GraphAnalyzer:
     """
     A class for analyzing and visualizing graphs.
     """
-    def __init__(self, edge_indices_file=None, nodes_file_name=None, dataset_name=None, generate_graphs=True):
+    def __init__(self, edge_indices_file=None, nodes_file_name=None, dataset_name=None, remove_zero_nodes=False, generate_graphs=True):
         """
         Initializes a GraphAnalyzer object with the given edge indices file, nodes file name, and dataset name.
 
@@ -21,21 +21,24 @@ class GraphAnalyzer:
         - edge_indices_file: str   - The path to the edge indices file
         - nodes_file_name: str     - The path to the nodes file
         - dataset_name: str        - The name of the dataset
-        - generate_graphs: bool    - Whether to generate the Erdos-Renyi, Barabasi-Albert, and Watts-Strogatz models
+        - remove_zero_nodes: bool  - Whether to remove nodes with degree 0
+        - generate_graphs: bool    - Whether to generate the Erdos-Renyi, Barabasi-Albert, Watts-Strogatz, and Powerlaw Cluster models.
         """
         self.edge_indices_file = edge_indices_file
         self.nodes_file_name = nodes_file_name
         self.dataset_name = dataset_name
+        self.remove_zero_nodes = remove_zero_nodes
         self.graph = None
         self.load_graph()
         if generate_graphs:
             self.er_model = self.generate_erdos_renyi()
             self.ba_model = self.generate_barabasi_albert()
             self.ws_model = self.generate_watts_strogatz()
+            self.pc_model = self.generate_powerlaw_cluster()
             self.graph_visualizer = GraphVisualizer(
-                [self.graph, self.er_model, self.ba_model, self.ws_model], 
-                [self.dataset_name, 'Erdos-Renyi', 'Barabasi-Albert', 'Watts-Strogatz'],
-                ['skyblue', 'lightcoral', 'lightgreen', 'pink'])
+                [self.graph, self.er_model, self.ba_model, self.ws_model, self.pc_model], 
+                [self.dataset_name, 'Erdos-Renyi', 'Barabasi-Albert', 'Watts-Strogatz', 'Powerlaw Cluster'],
+                ['skyblue', 'lightcoral', 'lightgreen', 'pink', 'lavender'])
 
     def load_graph(self):
         """
@@ -55,6 +58,11 @@ class GraphAnalyzer:
             for i in range(edge_indices.shape[1]):
                 source, target = edge_indices[:, i]
                 self.graph.add_edge(source, target)
+                             
+            # Remove nodes with degree 0
+            if self.remove_zero_nodes:
+                self.graph.remove_nodes_from(list(nx.isolates(self.graph)))
+                num_nodes = len(self.graph.nodes)
 
             # a sparse matrix in Compressed Sparse Row (CSR) format
             # loaded_graph_adj = nx.adjacency_matrix(loaded_graph)
@@ -102,7 +110,8 @@ class GraphAnalyzer:
         properties = {'Loaded Graph': self._calculate_graph_properties(self.graph),
                       'Erdos-Renyi': self._calculate_graph_properties(self.er_model),
                       'Barabasi-Albert': self._calculate_graph_properties(self.ba_model),
-                      'Watts-Strogatz': self._calculate_graph_properties(self.ws_model)
+                      'Watts-Strogatz': self._calculate_graph_properties(self.ws_model),
+                      'Powerlaw Cluster': self._calculate_graph_properties(self.pc_model)
                       }
         return properties
 
@@ -112,7 +121,6 @@ class GraphAnalyzer:
         """
         num_nodes = len(self.graph.nodes)
         # num_edges = len(self.graph.edges)
-        # er_model = nx.gnm_random_graph(num_nodes, num_edges)
         p = len(self.graph.edges) / (num_nodes * (num_nodes - 1) / 2)
         er_model = nx.erdos_renyi_graph(num_nodes, p)
         return er_model
@@ -120,6 +128,7 @@ class GraphAnalyzer:
     def generate_barabasi_albert(self, m=5):
         """
         Generate a Barabasi-Albert random graph with the same number of nodes and edges as the loaded graph.
+        - m: int - Number of edges to attach from a new node to existing nodes
         """
         num_nodes = len(self.graph.nodes)
         ba_model = nx.barabasi_albert_graph(num_nodes, m)
@@ -128,20 +137,36 @@ class GraphAnalyzer:
     def generate_watts_strogatz(self, k=4, p=0.2):
         """
         Generate a Watts-Strogatz random graph with the same number of nodes and edges as the loaded graph.
+        - k: int - Each node is joined with its k nearest neighbors in a ring topology.
+        - p: float - The probability of rewiring each edge.
         """
         num_nodes = len(self.graph.nodes)
         ws_model = nx.watts_strogatz_graph(num_nodes, k, p)
         return ws_model
+    
+    def generate_powerlaw_cluster(self, m=5, p=0.2):
+        """
+        Generate a powerlaw cluster graph with the same number of nodes and edges as the loaded graph.
+        - n: int - Number of nodes
+        - m: int - Number of random edges to add for each new node
+        - p: float - Probability of adding a triangle after adding a random edge
+        """
+        num_nodes = len(self.graph.nodes)
+        pc_model = nx.powerlaw_cluster_graph(num_nodes, m, p)
+        return pc_model
 
     def visualize_graph(self):
         """
         Visualize the loaded graph, Erdos-Renyi, Barabasi-Albert, and Watts-Strogatz models.
         """
-        _, ax = plt.subplots(2, 2, figsize=(12, 8))
-        self.graph_visualizer.plot_network_graph(ax[0, 0], graph_index=0, with_labels=False, node_size=10)
-        self.graph_visualizer.plot_network_graph(ax[0, 1], graph_index=1, with_labels=False, node_size=10)
-        self.graph_visualizer.plot_network_graph(ax[1, 0], graph_index=2, with_labels=False, node_size=10)
-        self.graph_visualizer.plot_network_graph(ax[1, 1], graph_index=3, with_labels=False, node_size=10)
+        _, ax = plt.subplots(1, 5, figsize=(12, 3))
+        self.graph_visualizer.plot_network_graph(ax[0], graph_index=0, with_labels=False, node_size=10)
+        self.graph_visualizer.plot_network_graph(ax[1], graph_index=1, with_labels=False, node_size=10)
+        self.graph_visualizer.plot_network_graph(ax[2], graph_index=2, with_labels=False, node_size=10)
+        self.graph_visualizer.plot_network_graph(ax[3], graph_index=3, with_labels=False, node_size=10)
+        self.graph_visualizer.plot_network_graph(ax[4], graph_index=4, with_labels=False, node_size=10)
+        plt.tight_layout()
+        plt.show()
 
     def visualize_distribution(self):
         """
@@ -163,12 +188,14 @@ class GraphAnalyzer:
         jaccard_er = nx.jaccard_coefficient(self.graph, self.er_model.edges())
         jaccard_ba = nx.jaccard_coefficient(self.graph, self.ba_model.edges())
         jaccard_ws = nx.jaccard_coefficient(self.graph, self.ws_model.edges())
+        jaccard_pc = nx.jaccard_coefficient(self.graph, self.pc_model.edges())
 
         # Extract the Jaccard similarity coefficients
         jaccard_coefficients = {
             'Erdos-Renyi': sum([coeff for _, _, coeff in jaccard_er]) / len(self.er_model.edges()),
             'Barabasi-Albert': sum([coeff for _, _, coeff in jaccard_ba]) / len(self.ba_model.edges()),
-            'Watts-Strogatz': sum([coeff for _, _, coeff in jaccard_ws]) / len(self.ws_model.edges())
+            'Watts-Strogatz': sum([coeff for _, _, coeff in jaccard_ws]) / len(self.ws_model.edges()),
+            'Powerlaw Cluster': sum([coeff for _, _, coeff in jaccard_pc]) / len(self.pc_model.edges())
             }
 
         return jaccard_coefficients
