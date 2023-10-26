ec0340d4705974ffeda19de5ee265582dae6067c
Harshit Kumar <harshit.kumar@jax.org>
2023-10-26 08:45:15 -0400

    Ensure motifs have same node features

--- src/graphgen/graph_with_motifs_dataset.py
+++ src/graphgen/graph_with_motifs_dataset.py
@@ -2,6 +2,7 @@ from .graph_with_motifs import GraphWithMotifs
 
 import random
 import pickle
+import numpy as np
 import networkx as nx
 
 class GraphWithMotifsDataset:
@@ -35,18 +36,27 @@ class GraphWithMotifsDataset:
         # TODO: Embed different number of motifs in graph 
         # a) non-overlapping classes b) overlapping (class decided based on probability)
         classes = 2
-        motif_adjacencies = []
-        for _ in range(classes):
+        motifs_dict = {}
+        for i in range(classes):
             # Generate a random motif size within the specified range
             motif_size = random.randint(self.min_motif_size, self.max_motif_size)
-            motif_adjacencies.append(nx.to_numpy_array(self.graph_with_motifs.create_graph_model(self.motif_graph_model, motif_size)))
+            motif_adj = nx.to_numpy_array(self.graph_with_motifs.create_graph_model(self.motif_graph_model, motif_size))
+            motif_features = []
+            # Get the indices of the non-zero elements in the matrix
+            row_indices, col_indices = np.where(motif_adj != 0)
+            # Combine the row and column indices to get a list of nodes
+            motif_nodes = list(set(row_indices) | set(col_indices))
+            for _ in motif_nodes:
+                motif_features.append(self.graph_with_motifs.assign_node_feature())
+            motifs_dict[f'motif_{i}'] = {'name': f'motif_{i}', 'adj_matrix': motif_adj, 'node_features': motif_features, 'class_label': i}
+            
 
         # Generate samples for each class
         for _ in range(self.num_graphs):
             # Create a graph with a positive motif
-            label = random.choice(range(classes))
-            graph_adj, motifs, node_features = self.graph_with_motifs.create_graph_with_motif_adjacency([motif_adjacencies[label]])
-            self.dataset.append((graph_adj, motifs, node_features, label))
+            motif_name = random.choice(list(motifs_dict.keys()))
+            graph_adj, motifs, node_features = self.graph_with_motifs.create_graph_with_motif_adjacency([motifs_dict[motif_name]])
+            self.dataset.append((graph_adj, motifs, node_features, motifs_dict[motif_name]['class_label']))
 
     def get_dataset(self):
         return self.dataset
